1e15d9705526149321312b6e54a0d1ad715b7cb3
prohubber <86630081+prohubber@users.noreply.github.com>
2024-10-21 04:25:38 +0700

    Обновление программы
    1. Добавлена глобальная переменная `message`
    2. Добавлен `POST` handler, который будет принимать json с полем `message` и записывать его содержимое в нашу переменную
    3. Обновлен `GET` handler, чтобы он возвращал “hello, `message` ”

--- main.go
+++ main.go
@@ -1,19 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+var message string
+
+type requestBody struct {
+	Message string `json:"message"`
+}
+
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World!")
+	fmt.Fprintf(w, "Hello, %s!", message)
+}
+
+func MessageHandler(w http.ResponseWriter, r *http.Request) {
+	var reqBody requestBody
+
+	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	message = reqBody.Message
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func main() {
 	router := mux.NewRouter()
-	// наше приложение будет слушать запросы на localhost:8080/api/hello
 	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+	router.HandleFunc("/api/message", MessageHandler).Methods("POST")
 	http.ListenAndServe(":8080", router)
 }

25231354fc99c8d81920e07f7af285d4d2c1ae24
prohubber <86630081+prohubber@users.noreply.github.com>
2024-11-29 01:15:38 +0700

    Обновление программы
    Добавлены ручки PATCH и DELETE

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -14,8 +15,8 @@ type Task struct {
 }
 
 type requestBody struct {
-	Task   string `json:"task"`
-	IsDone bool   `json:"is_done"`
+	Task   string `json:"task,omitempty"`
+	IsDone *bool  `json:"is_done,omitempty"`
 }
 
 func CreateMessage(w http.ResponseWriter, r *http.Request) {
@@ -27,8 +28,13 @@ func CreateMessage(w http.ResponseWriter, r *http.Request) {
 	}
 
 	task := Task{
-		Task:   reqBody.Task,
-		IsDone: reqBody.IsDone,
+		Task: reqBody.Task,
+		IsDone: func() bool {
+			if reqBody.IsDone != nil {
+				return *reqBody.IsDone
+			}
+			return false
+		}(),
 	}
 
 	if err := DB.Create(&task).Error; err != nil {
@@ -51,6 +57,58 @@ func GetMessages(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(tasks)
 }
 
+func UpdateMessage(w http.ResponseWriter, r *http.Request) {
+	var reqBody requestBody
+	vars := mux.Vars(r)
+	id, err := strconv.ParseUint(vars["id"], 10, 32)
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var task Task
+	if err := DB.First(&task, id).Error; err != nil {
+		http.Error(w, "Task not found", http.StatusNotFound)
+		return
+	}
+
+	if reqBody.Task != "" {
+		task.Task = reqBody.Task
+	}
+	if reqBody.IsDone != nil {
+		task.IsDone = *reqBody.IsDone
+	}
+
+	if err := DB.Save(&task).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(task)
+}
+
+func DeleteMessage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.ParseUint(vars["id"], 10, 32)
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+
+	if err := DB.Delete(&Task{}, id).Error; err != nil {
+		http.Error(w, "Task not found", http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
 func main() {
 	// Вызываем метод InitDB() из файла db.go
 	InitDB()
@@ -61,5 +119,7 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/messages", CreateMessage).Methods("POST")
 	router.HandleFunc("/api/messages", GetMessages).Methods("GET")
+	router.HandleFunc("/api/messages/{id}", UpdateMessage).Methods("PATCH")
+	router.HandleFunc("/api/messages/{id}", DeleteMessage).Methods("DELETE")
 	http.ListenAndServe(":8080", router)
 }
